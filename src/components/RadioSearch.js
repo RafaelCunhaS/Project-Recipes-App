799663c6be2b5f4ea276e1980f6d623f054fc3b5
Felipe Barboza <felipebarboza5@gmail.com>
2022-03-08 17:45:22 -0300

    Header-search concluido

--- src/components/RadioSearch.js
+++ src/components/RadioSearch.js
@@ -7,6 +7,47 @@ function RadioSearch() {
   const { pathname } = history.location;
   const { getPathName, getRadioValue, callApi } = useContext(apiContext);
 
+  const verifyRecipe = (data) => {
+    if (data === null) {
+      return global.alert('Sorry, we haven\'t found any recipes for these filters.');
+    }
+  };
+
+  const searchHelpFood = (value) => {
+    if (value.length === 1) {
+      const { idMeal } = value[0];
+      history.push(`/foods/${idMeal}`);
+    }
+  };
+
+  const searchHelpDrink = (value) => {
+    if (value.length === 1) {
+      const { idDrink } = value[0];
+      history.push(`/drinks/${idDrink}`);
+    }
+  };
+
+  const handleSearch = async () => {
+    if (pathname === '/foods') {
+      const data = await callApi();
+      if (data) {
+        verifyRecipe(data.meals);
+        if (data.meals !== null) {
+          searchHelpFood(data.meals);
+        }
+      }
+    }
+    if (pathname === '/drinks') {
+      const data = await callApi();
+      if (data) {
+        verifyRecipe(data.drinks);
+        if (data.drinks !== null) {
+          searchHelpDrink(data.drinks);
+        }
+      }
+    }
+  };
+
   useEffect(() => {
     getPathName(pathname);
   // eslint-disable-next-line react-hooks/exhaustive-deps
@@ -51,7 +92,7 @@ function RadioSearch() {
       </label>
 
       <button
-        onClick={ () => callApi() }
+        onClick={ handleSearch }
         type="button"
         data-testid="exec-search-btn"
       >
--- src/services/useApi.js
+++ src/services/useApi.js
@@ -1,17 +1,17 @@
 export const apiIngredients = async (path, input) => {
   const URL = `https://www.${path}.com/api/json/v1/1/filter.php?i=${input}`;
   const data = await fetch(URL).then((response) => response.json());
-  console.log(data);
+  return data;
 };
 
 export const apiName = async (path, input) => {
   const URL = `https://www.${path}.com/api/json/v1/1/search.php?s=${input}`;
   const data = await fetch(URL).then((response) => response.json());
-  console.log(data);
+  return data;
 };
 
 export const apiFirstLetter = async (path, input) => {
   const URL = `https://www.${path}.com/api/json/v1/1/search.php?f=${input}`;
   const data = await fetch(URL).then((response) => response.json());
-  console.log(data);
+  return data;
 };
